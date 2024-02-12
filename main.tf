f7e52f9ddab3c5aadbbe6bcc27adda808e3c981f
Avherrera <avillah@unsa.edu.pe>
2024-02-12 17:26:33 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,21 @@
 # Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.0.2"
+#     }
+#   }
+# }
 
-#   required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
+# provider "azurerm" {
+#   features {
+#     key_vault {
+#       purge_soft_delete_on_destroy    = true
+#       recover_soft_deleted_key_vaults = true
+#     }
+#   }
+# }
 
 # resource "azurerm_resource_group" "rg" {
 #   name     = "avhrg2"
@@ -116,34 +114,68 @@ variable "RG_name"{
 # }
 
 # key vault
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "example" {
-  name                        = "examplekeyvault"
-  location                    = "${var.location}"
-  resource_group_name         = "${var.RG_name}"
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+resource "random_string" "azurerm_key_vault_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
 
-  sku_name = "standard"
+locals {
+  current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
+}
+
+resource "azurerm_key_vault" "vault" {
+  name                       = coalesce(var.vault_name, "vault-${random_string.azurerm_key_vault_name.result}")
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = var.sku_name
+  soft_delete_retention_days = 7
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+    object_id = local.current_user_id
 
-    key_permissions = [
-      "Get",
-    ]
+    key_permissions    = var.key_permissions
+    secret_permissions = var.secret_permissions
+  }
+}
 
-    secret_permissions = [
-      "Get",
-    ]
+resource "random_string" "azurerm_key_vault_key_name" {
+  length  = 13
+  lower   = true
+  numeric = false
+  special = false
+  upper   = false
+}
 
-    storage_permissions = [
-      "Get",
-    ]
+resource "azurerm_key_vault_key" "key" {
+  name = coalesce(var.key_name, "key-${random_string.azurerm_key_vault_key_name.result}")
+
+  key_vault_id = azurerm_key_vault.vault.id
+  key_type     = var.key_type
+  key_size     = var.key_size
+  key_opts     = var.key_ops
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
+    }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
   }
 }
