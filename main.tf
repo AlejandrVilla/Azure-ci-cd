51fdaf18641a4ca39455e5d3489d65f79bec8262
Avherrera <avillah@unsa.edu.pe>
2024-02-13 02:20:46 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -1,112 +1,80 @@
-# Configure the Azure provider
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "~> 3.0.2"
-#     }
-#   }
-# }
+variable "prefix" {
+  default = "tfvm"
+}
 
-# provider "azurerm" {
-#   features {
-#     key_vault {
-#       purge_soft_delete_on_destroy    = true
-#       recover_soft_deleted_key_vaults = true
-#     }
-#   }
-# }
+variable "location"{
+  default = "West US 2"
+}
 
-# resource "azurerm_resource_group" "rg" {
-#   name     = "avhrg2"
-#   location = "westus2"
-# }
-
-# Create a virtual network
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "myTFVnet"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = "westus2"
-#   resource_group_name = "avhrg2"
-# }
-
-# variable "prefix" {
-#   default = "tfvm"
-# }
-
-# variable "location"{
-#   default = "West US 2"
-# }
-
-# variable "RG_name"{
-#   default = "avhrg2"
-# }
+variable "RG_name"{
+  default = "avhrg2"
+}
 
 # # Virtual Machine
 
-# resource "azurerm_virtual_network" "main" {
-#   name                = "${var.prefix}-network"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = "${var.location}"
-#   resource_group_name = "${var.RG_name}"
-# }
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${var.location}"
+  resource_group_name = "${var.RG_name}"
+}
 
-# resource "azurerm_subnet" "internal" {
-#   name                 = "internal"
-#   resource_group_name  = "${var.RG_name}"
-#   virtual_network_name = azurerm_virtual_network.main.name
-#   address_prefixes     = ["10.0.2.0/24"]
-# }
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${var.RG_name}"
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-# resource "azurerm_network_interface" "main" {
-#   name                = "${var.prefix}-nic"
-#   location            = "${var.location}"
-#   resource_group_name = "${var.RG_name}"
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${var.location}"
+  resource_group_name = "${var.RG_name}"
 
-#   ip_configuration {
-#     name                          = "testconfiguration1"
-#     subnet_id                     = azurerm_subnet.internal.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-# }
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
-# resource "azurerm_virtual_machine" "main" {
-#   name                  = "${var.prefix}-vm"
-#   location              = "${var.location}"
-#   resource_group_name   = "${var.RG_name}"
-#   network_interface_ids = [azurerm_network_interface.main.id]
-#   vm_size               = "Standard_DS1_v2"
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = "${var.location}"
+  resource_group_name   = "${var.RG_name}"
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
 
-#   # Uncomment this line to delete the OS disk automatically when deleting the VM
-#   delete_os_disk_on_termination = true
+  # delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
 
-#   # Uncomment this line to delete the data disks automatically when deleting the VM
-#   delete_data_disks_on_termination = true
+  # delete the data disks automatically when deleting the VM
+  delete_data_disks_on_termination = true
 
-#   storage_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-lts"
-#     version   = "latest"
-#   }
-#   storage_os_disk {
-#     name              = "myosdisk1"
-#     caching           = "ReadWrite"
-#     create_option     = "FromImage"
-#     managed_disk_type = "Standard_LRS"
-#   }
-#   os_profile {
-#     computer_name  = "hostname"
-#     admin_username = "testadmin"
-#     admin_password = "Password1234!"
-#   }
-#   os_profile_linux_config {
-#     disable_password_authentication = false
-#   }
-#   tags = {
-#     environment = "staging"
-#   }
-# }
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
 
 # # key vault
 resource "random_pet" "rg_name" {
