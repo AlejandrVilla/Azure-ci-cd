727450b875ad2ee3f11545f11a8508803b815d24
Avherrera <avillah@unsa.edu.pe>
2024-02-13 01:08:06 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -23,154 +23,154 @@
 # }
 
 # Create a virtual network
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "myTFVnet"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = "westus2"
-#   resource_group_name = "avhrg2"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = "avhrg2"
+}
+
+# variable "prefix" {
+#   default = "tfvm"
 # }
 
-variable "prefix" {
-  default = "tfvm"
-}
+# variable "location"{
+#   default = "West US 2"
+# }
 
-variable "location"{
-  default = "West US 2"
-}
+# variable "RG_name"{
+#   default = "avhrg2"
+# }
 
-variable "RG_name"{
-  default = "avhrg2"
-}
+# # Virtual Machine
 
-# Virtual Machine
+# resource "azurerm_virtual_network" "main" {
+#   name                = "${var.prefix}-network"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = "${var.location}"
+#   resource_group_name = "${var.RG_name}"
+# }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${var.location}"
-  resource_group_name = "${var.RG_name}"
-}
+# resource "azurerm_subnet" "internal" {
+#   name                 = "internal"
+#   resource_group_name  = "${var.RG_name}"
+#   virtual_network_name = azurerm_virtual_network.main.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = "${var.RG_name}"
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+# resource "azurerm_network_interface" "main" {
+#   name                = "${var.prefix}-nic"
+#   location            = "${var.location}"
+#   resource_group_name = "${var.RG_name}"
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = "${var.location}"
-  resource_group_name = "${var.RG_name}"
+#   ip_configuration {
+#     name                          = "testconfiguration1"
+#     subnet_id                     = azurerm_subnet.internal.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+# resource "azurerm_virtual_machine" "main" {
+#   name                  = "${var.prefix}-vm"
+#   location              = "${var.location}"
+#   resource_group_name   = "${var.RG_name}"
+#   network_interface_ids = [azurerm_network_interface.main.id]
+#   vm_size               = "Standard_DS1_v2"
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = "${var.location}"
-  resource_group_name   = "${var.RG_name}"
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+#   # Uncomment this line to delete the OS disk automatically when deleting the VM
+#   delete_os_disk_on_termination = true
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
+#   # Uncomment this line to delete the data disks automatically when deleting the VM
+#   delete_data_disks_on_termination = true
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  delete_data_disks_on_termination = true
+#   storage_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts"
+#     version   = "latest"
+#   }
+#   storage_os_disk {
+#     name              = "myosdisk1"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = "hostname"
+#     admin_username = "testadmin"
+#     admin_password = "Password1234!"
+#   }
+#   os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
-}
+# # key vault
+# resource "random_pet" "rg_name" {
+#   prefix = var.resource_group_name_prefix
+# }
 
-# key vault
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = random_pet.rg_name.id
+#   location = var.resource_group_location
+# }
 
-resource "azurerm_resource_group" "rg" {
-  name     = random_pet.rg_name.id
-  location = var.resource_group_location
-}
+# data "azurerm_client_config" "current" {}
 
-data "azurerm_client_config" "current" {}
+# resource "random_string" "azurerm_key_vault_name" {
+#   length  = 13
+#   lower   = true
+#   numeric = false
+#   special = false
+#   upper   = false
+# }
 
-resource "random_string" "azurerm_key_vault_name" {
-  length  = 13
-  lower   = true
-  numeric = false
-  special = false
-  upper   = false
-}
+# locals {
+#   current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
+# }
 
-locals {
-  current_user_id = coalesce(var.msi_id, data.azurerm_client_config.current.object_id)
-}
+# resource "azurerm_key_vault" "vault" {
+#   name                       = coalesce(var.vault_name, "vault-${random_string.azurerm_key_vault_name.result}")
+#   location                   = azurerm_resource_group.rg.location
+#   resource_group_name        = azurerm_resource_group.rg.name
+#   tenant_id                  = data.azurerm_client_config.current.tenant_id
+#   sku_name                   = var.sku_name
+#   soft_delete_retention_days = 7
 
-resource "azurerm_key_vault" "vault" {
-  name                       = coalesce(var.vault_name, "vault-${random_string.azurerm_key_vault_name.result}")
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  tenant_id                  = data.azurerm_client_config.current.tenant_id
-  sku_name                   = var.sku_name
-  soft_delete_retention_days = 7
+#   access_policy {
+#     tenant_id = data.azurerm_client_config.current.tenant_id
+#     object_id = local.current_user_id
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = local.current_user_id
+#     key_permissions    = var.key_permissions
+#     secret_permissions = var.secret_permissions
+#   }
+# }
 
-    key_permissions    = var.key_permissions
-    secret_permissions = var.secret_permissions
-  }
-}
+# resource "random_string" "azurerm_key_vault_key_name" {
+#   length  = 13
+#   lower   = true
+#   numeric = false
+#   special = false
+#   upper   = false
+# }
 
-resource "random_string" "azurerm_key_vault_key_name" {
-  length  = 13
-  lower   = true
-  numeric = false
-  special = false
-  upper   = false
-}
+# resource "azurerm_key_vault_key" "key" {
+#   name = coalesce(var.key_name, "key-${random_string.azurerm_key_vault_key_name.result}")
 
-resource "azurerm_key_vault_key" "key" {
-  name = coalesce(var.key_name, "key-${random_string.azurerm_key_vault_key_name.result}")
+#   key_vault_id = azurerm_key_vault.vault.id
+#   key_type     = var.key_type
+#   key_size     = var.key_size
+#   key_opts     = var.key_ops
 
-  key_vault_id = azurerm_key_vault.vault.id
-  key_type     = var.key_type
-  key_size     = var.key_size
-  key_opts     = var.key_ops
+#   rotation_policy {
+#     automatic {
+#       time_before_expiry = "P30D"
+#     }
 
-  rotation_policy {
-    automatic {
-      time_before_expiry = "P30D"
-    }
-
-    expire_after         = "P90D"
-    notify_before_expiry = "P29D"
-  }
-}
+#     expire_after         = "P90D"
+#     notify_before_expiry = "P29D"
+#   }
+# }
