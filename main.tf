ae7f30f8231df2ddc3ca9dde6fa131dbb8a329ce
Avherrera <avillah@unsa.edu.pe>
2024-02-13 01:16:46 -0500

    main.tf

--- main.tf
+++ main.tf
@@ -109,9 +109,9 @@ resource "azurerm_virtual_network" "vnet" {
 # }
 
 # # key vault
-# resource "random_pet" "rg_name" {
-#   prefix = var.resource_group_name_prefix
-# }
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg_name.id
